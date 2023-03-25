a37c3baa3665bf648d0275949521c31c2072f91f
İbrahim Baymaz <20200808006@ogr.akdeniz.edu.tr>
2023-03-25 23:26:13 +0300

    Result text can be shown in the screen.

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -13,9 +14,23 @@ class MainActivity : AppCompatActivity() {
         val firstDice = Dice(6)
 
         val rollButton : Button = findViewById(R.id.rollButton)
+        val resultText : TextView = findViewById(R.id.resultTextDice)
+
         rollButton.setOnClickListener{
             Toast.makeText(this,"Dice Rolled! \uD83C\uDFB2 ",Toast.LENGTH_SHORT).show()
+
+            when(firstDice.rollDice()){
+                1 -> resultText.text = "1"
+                2 -> resultText.text = "2"
+                3 -> resultText.text = "3"
+                4 -> resultText.text = "4"
+                5 -> resultText.text = "5"
+                6 -> resultText.text = "6"
+            }
         }
+
+
+
     }
 
 
