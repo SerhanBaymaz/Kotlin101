72ee7ebf6df4fae4d1be4a2920732df083a173ec
İbrahim Baymaz <20200808006@ogr.akdeniz.edu.tr>
2023-03-31 20:46:02 +0300

    Radio group created and radio buttons added to layout.

--- TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val calculateButton : Button = findViewById(R.id.idButtonCalculate)
+        calculateButton.setOnClickListener{
+            Toast.makeText(this,"Calculated.",Toast.LENGTH_SHORT).show()
+
+
+        }
     }
 }
