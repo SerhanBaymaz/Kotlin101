7dc7314c2094f00bc214ffb96741a6de4d5e98ec
İbrahim Baymaz <20200808006@ogr.akdeniz.edu.tr>
2023-04-09 02:48:18 +0300

    Functions implemented, images are clickable now.

--- Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -6,33 +6,53 @@ import com.example.lemonade.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    var lemodaneCount =0
+    var lemodaneCount = 0
 
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.idTextView.text="Pick the lemon from tree"
+        pickLemon()
+
+    }
+
+    fun pickLemon() {
+        binding.idTextView.text = "Pick the lemon from tree"
         binding.idImageView.setImageResource(R.drawable.lemon_tree)
 
+        binding.idImageView.setOnClickListener {
+            squezeLemon()
+        }
     }
 
-    fun pickLemon(){
+    fun squezeLemon() {
+        binding.idTextView.text = "Squeze the lemon"
+        binding.idImageView.setImageResource(R.drawable.lemon_squeeze)
 
+        binding.idImageView.setOnClickListener {
+            drinkLemonade()
+        }
     }
 
-    fun squezeLemon(){
+    fun drinkLemonade() {
+        binding.idTextView.text = "Drink the lemonade"
+        binding.idImageView.setImageResource(R.drawable.lemon_drink)
+        lemodaneCount++
 
+        binding.idImageView.setOnClickListener {
+            rePickLemon()
+        }
     }
 
-    fun drinkLemonade(){
+    fun rePickLemon() {
+        binding.idTextView.text = "Go to the lemon tree"
+        binding.idImageView.setImageResource(R.drawable.lemon_restart)
 
-    }
-
-    fun rePickLemon(){
-
+        binding.idImageView.setOnClickListener {
+            pickLemon()
+        }
     }
 
 }
